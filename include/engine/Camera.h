38a4de8f8e362140e2aa6e2071fa40e8097a32bf
Author Name <beragnoli.jacopo@studenti-ittfedifermi.edu.it>
2022-04-15 15:10:23 +0200

    Camera movement Keyboard Listener

--- include/engine/Camera.h
+++ include/engine/Camera.h
@@ -17,10 +17,18 @@ namespace Engine {
 
         void setProspectiveProjection(float fovy, float aspect, float near, float far);
 
-        const glm::mat4 &getProjection() const { return projectionMatrix; }
+        void setViewDirection(glm::vec3 position, glm::vec3 direction, glm::vec3 up = glm::vec3{.0f, -1.f, .0f});
+
+        void setViewTarget(glm::vec3 position, glm::vec3 target, glm::vec3 up = glm::vec3{.0f, -1.f, .0f});
+        void setViewYXZ(glm::vec3 position, glm::vec3 rotation);
+
+        const glm::mat4 &getProjection() const;
+
+        const glm::mat4 &getView() const;
 
     private:
         glm::mat4 projectionMatrix{1.f};
+        glm::mat4 viewMatrix{1.f};
     };
 }
 
---
+++ include/engine/KeyboardMovementController.h
@@ -0,0 +1,37 @@
+//
+// Created by Jacopo Beragnoli on 15/04/22.
+//
+
+#ifndef MINIMINIMOTORWAYS_KEYBOARDMOVEMENTCONTROLLER_H
+#define MINIMINIMOTORWAYS_KEYBOARDMOVEMENTCONTROLLER_H
+
+#include <GLFW/glfw3.h>
+#include "GameObject.h"
+
+namespace Engine {
+    class KeyboardMovementController {
+    public:
+        struct KeyMappings {
+            int moveLeft = GLFW_KEY_A;
+            int moveRight = GLFW_KEY_D;
+            int moveForward = GLFW_KEY_W;
+            int moveBackward = GLFW_KEY_S;
+            int moveUp = GLFW_KEY_E;
+            int moveDown = GLFW_KEY_Q;
+            int lookLeft = GLFW_KEY_LEFT;
+            int lookRight = GLFW_KEY_RIGHT;
+            int lookUp = GLFW_KEY_UP;
+            int lookDown = GLFW_KEY_DOWN;
+        };
+
+        void moveInPlaneXZ(GLFWwindow *glfWwindow, float dt, Engine::GameObject &gameObject);
+
+        KeyMappings keys{};
+        float moveSpeed{3.f};
+        float lookSpeed{1.5f};
+    private:
+    };
+}
+
+
+#endif //MINIMINIMOTORWAYS_KEYBOARDMOVEMENTCONTROLLER_H
